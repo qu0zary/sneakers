a211d628f01bc7e99587a47bac40ac26a2f83e0d
quozary <gabdrakhmanov.arthur@gmail.com>
2025-03-11 20:12:32 +0700

    first commit

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      'https://e59eaa734d2474ce.mokky.dev/favorites?_relations=items',
+    )
+    favorites.value = data.map((obj) => obj.item)
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h1>Мои закладки</h1>
+  <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+
+  <CardList :items="favorites" is-favorites />
+</template>
